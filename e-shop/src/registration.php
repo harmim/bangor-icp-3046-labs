cab43ff835af88353d4f7f4f689ea231c7a9b14a
Dominik Harmim <harmim6@gmail.com>
2018-03-03 23:45:32 +0000

    Lab 5, 6, 7

--- e-shop/src/registration.php
+++ e-shop/src/registration.php
@@ -9,15 +9,55 @@
 declare(strict_types=1);
 
 use Main\Configuration;
-use Main\Renderable\PersonalInformationForm;
+use Main\Renderable;
+use Main\Security;
+use Main\Service;
 
 
 require_once __DIR__ . '/includes/configuration.php';
+
+
 Configuration::setTitleSection('Registration');
 
+// redirect user to personal information page if he is already logged in
+if (Configuration::getLoggedUser()) {
+	Configuration::redirect('personalInformation.php');
+}
 
-// TODO: Redirect away if user is already logged in.
-// TODO: Process, fill and validate registration form.
+/** @var Service\UserService $userService */
+$userService = Configuration::getService(Service\UserService::class);
+$values = Configuration::getHttpRequest()->getPost();
+if (isset($values['submit'])) {
+	// validation
+	if (isset($values['email'], $values['forename'], $values['surname'], $values['password'], $values['confirmPassword'])) {
+		try {
+			$userService->createUser(
+				$values['email'],
+				$values['forename'],
+				$values['surname'],
+				$values['password'],
+				$values['confirmPassword']
+			);
+
+			Configuration::setLoggedUser((new Security\Authenticator())->authenticate($values['email'], $values['password']));
+			Renderable\Messages::addMessage(
+				'You have been successfully registered and logged in.',
+				Renderable\Messages::TYPE_SUCCESS
+			);
+
+			Configuration::redirect('index.php');
+
+		} catch (UnexpectedValueException $e) {
+			Renderable\Messages::addMessage($e->getMessage(), Renderable\Messages::TYPE_DANGER);
+		}
+
+	} else {
+		Renderable\Messages::addMessage(
+			'Please enter all required fields.',
+			Renderable\Messages::TYPE_DANGER
+		);
+	}
+}
 
 siteHeader();
 
@@ -29,7 +69,13 @@ siteHeader();
 
 		<?php
 
-		$registrationForm = new PersonalInformationForm('registration.php', [
+		$registrationForm = new Renderable\PersonalInformationForm('registration.php', [
+			'forename' => [
+				'value' => $values['forename'] ?? null,
+			],
+			'surname' => [
+				'value' => $values['surname'] ?? null,
+			],
 			'password' => [
 				'required' => true,
 			],
